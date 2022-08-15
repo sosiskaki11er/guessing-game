889da69fe673f92d02215b47e9a35338a2b2e1fa
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-08-16 03:23:12 +0500

    feat:function body added

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
+    array = [];
+    number;
     setRange(min, max) {
-
+        for (let i = min + 1; i < max; i++) {
+            this.array.push(i);
+        }
+        this.number = Math.ceil((this.array.length - 1) / 2);
     }
-
     guess() {
-
+        return this.array[this.number];
     }
-
     lower() {
-
+        this.array = this.array.slice(0, this.number);
+        this.number = Math.floor(this.array.length / 2);
     }
-
     greater() {
-
+        this.array = this.array.slice(this.number + 1);
+        this.number = Math.floor(this.array.length / 2);
     }
 }
 
